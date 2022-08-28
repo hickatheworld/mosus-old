6885453226e0b01823daa85cd37b7a7b993933ab
hickatheworld <cc@hicka.world>
2022-08-28 10:47:38 +0200

    feat: make slah commands Guild commands

--- src/register.ts
+++ src/register.ts
@@ -17,6 +17,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN!);
 (async () => {
 	// Deletes all commands to get rid of potentially deleted commands from the codebase. 
 	await rest.put(Routes.applicationCommands(process.env.APPLICATION_ID!), { body: [] });
-	await rest.put(Routes.applicationCommands(process.env.APPLICATION_ID!), { body: commands });
+	await rest.put(Routes.applicationGuildCommands(process.env.APPLICATION_ID!, process.env.GUILD_ID!), { body: [] });
+	await rest.put(Routes.applicationGuildCommands(process.env.APPLICATION_ID!, process.env.GUILD_ID!), { body: commands });
 	console.log('Successfully registered commands.');
 })();
